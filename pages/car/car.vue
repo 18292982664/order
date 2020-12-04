8cf38c07e4d18fef60fed4effcbbc1fc1c0172ee
18292982664 <36914185+18292982664@users.noreply.github.com>
2020-12-04 18:13:50 +0800

    first

---
+++ pages/car/car.vue
@@ -0,0 +1,15 @@
+<template>
+	<view>购物车</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style></style>
---
+++ pages/order/order.vue
@@ -0,0 +1,15 @@
+<template>
+	<view>订单</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style></style>
